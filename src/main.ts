4343296e55d2e3d65607f8e1829a12fa6bc4155a
Ross <rostishka1@gmail.com>
2022-12-04 15:54:51 +0200

    Added scalable function

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,23 @@
-import './style.css'
+import "./style.css";
 
+const WORLD_WIDTH = 100;
+const WORLD_HEIGHT = 30;
 
+const worldElem = document.querySelector("[data-world") as HTMLElement;
+
+const setPixelToWorldScale = () => {
+  let worldToPixelScale;
+
+  if ( window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
+    worldToPixelScale = window.innerWidth / WORLD_WIDTH
+  } else {
+    worldToPixelScale = window.innerHeight / WORLD_HEIGHT
+  }
+
+  worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`
+  worldElem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`
+
+}
+
+setPixelToWorldScale();
+window.addEventListener('resize', setPixelToWorldScale)
