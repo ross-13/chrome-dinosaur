5707880c8069349809fcaa12501a4f944cc142b8
Ross <rostishka1@gmail.com>
2022-12-04 16:16:21 +0200

    Implemented loop functionality

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import "./style.css";
 
 const WORLD_WIDTH = 100;
 const WORLD_HEIGHT = 30;
+// let speedScale;
+// let score;
 
 const worldElem = document.querySelector("[data-world") as HTMLElement;
 
-const setPixelToWorldScale = () => {
-  let worldToPixelScale;
+const setPixelToWorldScale = ():void => {
+  let worldToPixelScale: number;
 
   if ( window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
     worldToPixelScale = window.innerWidth / WORLD_WIDTH
@@ -18,6 +20,22 @@ const setPixelToWorldScale = () => {
   worldElem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`
 
 }
+
+let lastTime:null | number;
+const update = (time: number):void => {
+  if (lastTime == null ) {
+    lastTime = time;
+    window.requestAnimationFrame(update);
+    return;  
+  }
+  const delta = time - lastTime;
+  console.log(delta);
+
+  lastTime = time;
+  window.requestAnimationFrame(update);
+}
+
+window.requestAnimationFrame(update);
 
 setPixelToWorldScale();
 window.addEventListener('resize', setPixelToWorldScale)
