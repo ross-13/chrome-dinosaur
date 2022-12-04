3de829ef32d7641a9e748853840042cf9e2d3e5a
Ross <rostishka1@gmail.com>
2022-12-04 17:32:33 +0200

    Adding score

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,67 @@
 import "./style.css";
+import { updateGround, setupGround } from "./ground";
 
 const WORLD_WIDTH = 100;
 const WORLD_HEIGHT = 30;
-// let speedScale;
-// let score;
+const SPEED_SCALE_INCREASE = 0.0001; // TODO: Now it doesn't work
+
+let lastTime: null | number;
+let speedScale: number;
+let score: number;
 
 const worldElem = document.querySelector("[data-world") as HTMLElement;
+const scoreElem = document.querySelector("[data-score]") as HTMLElement;
+const startScreenElem = document.querySelector(
+  "[data-start-screen]"
+) as HTMLElement;
 
-const setPixelToWorldScale = ():void => {
+const setPixelToWorldScale = (): void => {
   let worldToPixelScale: number;
 
-  if ( window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
-    worldToPixelScale = window.innerWidth / WORLD_WIDTH
+  if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
+    worldToPixelScale = window.innerWidth / WORLD_WIDTH;
   } else {
-    worldToPixelScale = window.innerHeight / WORLD_HEIGHT
+    worldToPixelScale = window.innerHeight / WORLD_HEIGHT;
   }
 
-  worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`
-  worldElem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`
+  worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`;
+  worldElem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`;
+};
 
-}
+const updateSpeedScale = (delta: number): void => {
+  speedScale += delta * SPEED_SCALE_INCREASE;
+};
 
-let lastTime:null | number;
-const update = (time: number):void => {
-  if (lastTime == null ) {
+const updateScore = (delta: number): void => {
+  score += delta * 0.01;
+  scoreElem.textContent = Math.floor(score) + "";
+};
+
+const update = (time: number): void => {
+  if (lastTime == null) {
     lastTime = time;
     window.requestAnimationFrame(update);
-    return;  
+    return;
   }
   const delta = time - lastTime;
-  console.log(delta);
+
+  updateGround(delta, 1);
+  updateSpeedScale(delta);
+  updateScore(delta);
 
   lastTime = time;
   window.requestAnimationFrame(update);
-}
+};
 
-window.requestAnimationFrame(update);
+const start = () => {
+  lastTime = null;
+  speedScale = 1;
+  score = 0;
+  setupGround();
+  startScreenElem.classList.add("hide");
+  window.requestAnimationFrame(update);
+};
 
 setPixelToWorldScale();
-window.addEventListener('resize', setPixelToWorldScale)
+window.addEventListener("resize", setPixelToWorldScale);
+window.addEventListener("keydown", start, { once: true });
